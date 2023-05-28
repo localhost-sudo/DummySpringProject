3c27739228d6d23f2aa20afbb5f64bc248cc7fd2
suraj <suraj591@gmail.com>
2023-05-28 17:56:31 +0530

    by me

--- DummySpringProject/src/main/java/com/dummy/DummySpringProjectApplication.java
+++ DummySpringProject/src/main/java/com/dummy/DummySpringProjectApplication.java
@@ -11,6 +11,7 @@ public class DummySpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DummySpringProjectApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
